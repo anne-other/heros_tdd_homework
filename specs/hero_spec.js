61abe8f6747c1504155e402022251fa0720c646f
anne-other <anne.c.ince@gmail.com>
2018-08-21 18:13:53 +0100

    hero has health

--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -3,7 +3,7 @@ const Hero = require("../hero.js");
 
 describe("Hero", function(){
   beforeEach(function(){
-    hero = new Hero("Grogg Strongjaw")
+    hero = new Hero("Grogg Strongjaw", 200)
   });
 
   it("should have a name", function(){
@@ -11,7 +11,11 @@ describe("Hero", function(){
     assert.strictEqual(actual, "Grogg Strongjaw");
   });
 
-  xit("should have health");
+  it("should have health", function(){
+    const actual = hero.health;
+    assert.strictEqual(actual, 200);
+  });
+
   xit("should have a favourite food");
   xit("should talk saying their name");
   xit("should have a collection of tasks to complete");
