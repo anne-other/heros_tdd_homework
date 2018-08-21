5fb2380611698124e5aa9f2a8360aed2af30f26b
anne-other <anne.c.ince@gmail.com>
2018-08-21 21:02:51 +0100

    task has urgency

--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -7,7 +7,7 @@ let task
 describe("Hero", function(){
   beforeEach(function(){
     const task1 = new Task(1, 2)
-    const task2 = new Task(5, 1)
+    const task2 = new Task(5, 6)
     hero = new Hero("Grogg Strongjaw", 200, "Ale", [task1, task2])
   });
 
